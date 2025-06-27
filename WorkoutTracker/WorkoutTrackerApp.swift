946f03b4859637803616d5252393ed67454e2361
Tung Nguyen <tungnguyen1651@gmail.com>
2025-06-27 20:54:04 +0700

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Tung Nguyen on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutTrackerTests/WorkoutTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutTrackerTests.swift
+//  WorkoutTrackerTests
+//
+//  Created by Tung Nguyen on 6/27/25.
+//
+
+import Testing
+
+struct WorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
